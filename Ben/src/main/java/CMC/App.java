3471e1a199e9672c95ec90a4184a8cc68451b68f
MinhAnh24297 <MinhAnh.24297@gmail.com>
2022-11-24 22:26:52 +0700

    push code ver 2

--- Ben/src/main/java/CMC/App.java
+++ Ben/src/main/java/CMC/App.java
@@ -15,8 +15,13 @@ public class App
         StudentService studentService = new StudentService();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Chào mừng bạn tới với phần mềm, mời chọn option");
+        System.out.println("Chào mừng bạn tới với phần mềm");
         while (true) {
+            System.out.println("Có 2 option");
+            System.out.println("1. Thêm thông tin sinh viên");
+            System.out.println("2. Lấy ra danh sách sinh viên");
+            System.out.println("3. Kết thúc chương trình");
+            System.out.println("Mời chọn option");
             int choose = scanner.nextInt();
             switch (choose) {
                 case 1:
